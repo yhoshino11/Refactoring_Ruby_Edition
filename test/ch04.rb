5da4ae0d7c53ee15266bd01ca9dc90ef290e0f08
Yu Hoshino <yhoshino11@gmail.com>
2015-05-13 22:19:47 +0900

    Moved CH04 files to test/

--- test/ch04.rb
+++ test/ch04.rb
@@ -5,4 +5,9 @@ class TestCase < Minitest::Test
     contents = File.read('data.txt')
     assert_equal 'd', contents[3, 1]
   end
+
+  def test_read_4th_content_is_2
+    contents = File.read('data.txt')
+    assert_equal '2', contents[3, 1]
+  end
 end
