68aabffef5c509fa7edc14aefb5c65ce495bd569
Yu Hoshino <yhoshino11@gmail.com>
2015-05-13 18:00:48 +0900

    Moved Customer#amount_for to Rental#charge .

--- lib/ch01.rb
+++ lib/ch01.rb
@@ -17,6 +17,22 @@ class Rental
   def initialize(movie, days_rented)
     @movie, @days_rented = movie, days_rented
   end
+
+  def charge
+    result = 0
+    # determine amounts for each line
+    case movie.price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
 end
 
 class Customer
@@ -57,18 +73,6 @@ class Customer
   end
 
   def amount_for(rental)
-    result = 0
-    # determine amounts for each line
-    case rental.movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      result += rental.days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
-    result
+    rental.charge
   end
 end
