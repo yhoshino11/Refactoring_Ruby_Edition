b31f2e02261d623c4ff9c9850f05fb8004249ba5
Yu Hoshino <yhoshino11@gmail.com>
2015-05-13 13:14:54 +0900

    Add Customer's initial statement spec

--- ch01_spec.rb
+++ ch01_spec.rb
@@ -37,7 +37,12 @@ describe 'ch01' do
       expect(customer.name).to eq(name)
     end
 
-    let(:response) { "Rental Record for#{name}\n\t#{title}\t3.5\nAmount owed is 3.5\nYou earned 1 frequent renter points" }
+    let(:initial_response) { "Rental Record for#{name}\nAmount owed is 0\nYou earned 0 frequent renter points" }
+    it 'initial statement' do
+      expect(customer.statement).to eq(initial_response)
+    end
+
+    let(:response)         { "Rental Record for#{name}\n\t#{title}\t3.5\nAmount owed is 3.5\nYou earned 1 frequent renter points" }
     it 'add rental' do
       customer.add_rental(rental)
       expect(customer.statement).to eq(response)
