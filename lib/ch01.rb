b23b740c40c6faf3f4a54c536ac44ec1571e4c4e
Yu Hoshino <yhoshino11@gmail.com>
2015-05-13 18:13:55 +0900

    Calculate frequent renter points in Rental#frequent_renter_points .

--- lib/ch01.rb
+++ lib/ch01.rb
@@ -33,6 +33,10 @@ class Rental
     end
     result
   end
+
+  def frequent_renter_points
+    (movie.price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
 end
 
 class Customer
@@ -52,12 +56,7 @@ class Customer
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
       # add frequent renter points
-      frequent_renter_points += 1
-
-      # add bonus for a two day new release rental
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-        frequent_renter_points += 1
-      end
+      frequent_renter_points += element.frequent_renter_points
 
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
